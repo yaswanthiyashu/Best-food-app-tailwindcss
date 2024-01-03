170b2242edce9d8eee63d769f60ae7cb1f3f2b78
yaswanthiyashu <107607008+yaswanthiyashu@users.noreply.github.com>
2024-01-03 16:41:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Category from "./components/Category";
+import Food from "./components/Food";
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
